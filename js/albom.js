c4492d7146a413ded86c35a8579ef62e35804772
natalia646 <nataxash46@gmail.com>
2023-02-12 11:36:49 +0200

    Fetch/AJAX

--- js/albom.js
+++ js/albom.js
@@ -1,1 +1,40 @@
+//!Fetch
+fetch("https://jsonplaceholder.typicode.com/albums")
+  .then((response) => response.json())
+  .then((alboms) => {
+    for (let user of alboms) {
+      let h2 = document.createElement("h2");
+      h2.innerText = user.title;
+      // document.body.appendChild(h2)
+    }
+  });
 
+async function getData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/albums");
+  const alboms = await response.json();
+  for (let user of alboms) {
+    let h2 = document.createElement("h2");
+    h2.innerText = user.title;
+    document.body.appendChild(h2);
+  }
+}
+// getData();
+
+
+//!AJAX
+function initData(){
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4 && xhr.status === 200){
+         const alboms = JSON.parse(xhr.responseText);
+         for (let user of alboms) {
+            let h2 = document.createElement("h2");
+            h2.innerText = user.title;
+            document.body.appendChild(h2);
+          }
+        } 
+    }
+    xhr.open('GET', "https://jsonplaceholder.typicode.com/albums", true);
+    xhr.send()
+}
+initData()
